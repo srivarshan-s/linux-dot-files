dbe76a0ca66d8b9a34b8ffe87b6640bd76979223
Srivarshan <srivarshan02@gmail.com>
2023-01-26 21:34:00 +0530

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Install bash config
+ln -nfs $(pwd)/.bashrc ~/.
+
+# Install fonts
+ln -nfs $(pwd)/.fonts ~/.
+
+# Install git configs
+cp .git-credentials ~/.
+ln -nfs $(pwd)/.gitconfig ~/.
+
+# Install tmux config
+ln -nfs $(pwd)/.tmux.conf ~/.
+
+# Install alacritty config
+ln -nfs $(pwd)/alacritty ~/.config/.
+
+# Install starship config
+ln -nfs $(pwd)/starship.toml ~/.config/.
+
